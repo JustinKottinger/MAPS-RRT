7425a755064d67fb7bcec1b2215ad57b39c8e703
JustinKottinger <j.kottinger18@gmail.com>
2020-06-06 08:57:37 -0600

    improved functionality s.t. building is not required each time we want to run a different experiment

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include "../includes/CreateSimpleSetup.h"
 #include "../includes/KinematicCar.h"
 #include "../includes/MyPlanner.h"
+#include "../includes/MyPlannerMotion.h"
 #include "../includes/MAPSRRTPathControl.h"
 #include "ompl/tools/benchmark/Benchmark.h"
 #include "../includes/RRTplus.h"
@@ -214,100 +215,11 @@ class MyArbitraryGoal : public ompl::base::Goal
     }
 };
 
-
-
-
-
-
-// class MyGoalRegion : public ompl::base::GoalRegion 
-// {
-// public:
-//   MyGoalRegion(const ob::SpaceInformationPtr &si, std::vector<double> _goal, const double _toll) : ob::GoalRegion(si)
-
-//   {
-//     goal = _goal;  // [x1, y1, th1, x2, y2, th2]
-//     toll = _toll;
-//   }
-//   std::vector<double> goal;
-//   double toll;
-
-//   virtual bool isSatisfied(const ob::State *st) const
-//   {
-//     std::cout << "here" << std::endl;
-//     std::vector<double> d = TwoVehicleDistance(st);
-
-//     if (d[1] <= toll && d[2] <= toll)
-//     {
-//       MyPrint(d);
-//       return true;
-//     }
-//     else
-//       return false;
-//   }
-
-//   virtual std::vector<double> TwoVehicleDistance(const ob::State *st) const
-//   {
-//     std::vector<double> distances;
-//     auto cs_ = st->as<ompl::base::CompoundStateSpace::StateType>();
-//     auto xyState1_ = cs_->as<ob::RealVectorStateSpace::StateType>(0);
-//     auto xyState2_ = cs_->as<ob::RealVectorStateSpace::StateType>(2);
-
-//     double deltax_v1 = pow((goal[0] - xyState1_->values[0]), 2);
-//     double deltay_v1 = pow((goal[1] - xyState1_->values[1]), 2);
-//     double deltax_v2 = pow((goal[3] - xyState2_->values[0]), 2);
-//     double deltay_v2 = pow((goal[4] - xyState2_->values[1]), 2);
-
-//     double d1 = sqrt(deltax_v1 + deltay_v1);
-//     double d2 = sqrt(deltax_v2 + deltay_v2);
-
-//     distances.push_back(d1);
-//     distances.push_back(d2);
-//     return distances;
-//   }
-
-//   virtual double distanceGoal(const ob::State *st) const
-//   {
-//     // std::cout << "in my goal region" << std::endl;
-//     // do some stuff that returns the distnace to goal
-//     auto cs_ = st->as<ompl::base::CompoundStateSpace::StateType>();
-//     auto xyState1_ = cs_->as<ob::RealVectorStateSpace::StateType>(0);
-//     auto xyState2_ = cs_->as<ob::RealVectorStateSpace::StateType>(2);
-
-//     double deltax_v1 = pow((goal[0] - xyState1_->values[0]), 2);
-//     double deltay_v1 = pow((goal[1] - xyState1_->values[1]), 2);
-//     double deltax_v2 = pow((goal[3] - xyState2_->values[0]), 2);
-//     double deltay_v2 = pow((goal[4] - xyState2_->values[1]), 2);
-
-//     double d1 = sqrt(deltax_v1 + deltay_v1);
-//     double d2 = sqrt(deltax_v2 + deltay_v2);
-
-//     // double d = sqrt(deltax_v1 + deltay_v1 + deltax_v2 + deltay_v2);
-//     // std::cout << "beginning calc.." << std::endl;
-//     // MyPrint(goal);
-//     // std::cout << xyState1_->values[0] << " " << xyState1_->values[1] << std::endl;
-//     // std::cout << xyState2_->values[0] << " " << xyState2_->values[1] << std::endl;
-//     // std::cout << d1 << " " << d2 << std::endl;
-
-//     if ((d1 + d2) <= 2)
-//     {
-//       MyPrint(goal);
-//       std::cout << xyState1_->values[0] << " " << xyState1_->values[1] << std::endl;
-//       std::cout << xyState2_->values[0] << " " << xyState2_->values[1] << std::endl;
-//       std::cout << d1 << " " << d2 << std::endl;
-//     }
-
-//     return d1 + d2;
-//     // std::cout << d << std::endl;
-//     // return d;
-
-//   }
-// };
-
-
-void plan(std::vector<double> bndry, std::vector<double> gol, 
+void plan(std::string planner, std::vector<double> bndry, std::vector<double> gol, 
     std::vector<double> obs, std::vector<double> start, std::string model, 
     const double planningTime, const double Radius, std::vector<std::string> data, 
-    const int NumberofVehicles, const int NumberofControls, const int DimensionOfVehicle, int MaxSegments)
+    const int NumberofVehicles, const int NumberofControls, const int DimensionOfVehicle, 
+    int MaxSegments, bool benchmark)
 {
     oc::SimpleSetupPtr ss;
     CreateSimpleSetup(ss, bndry, gol, start, model, Radius);
@@ -331,117 +243,162 @@ void plan(std::vector<double> bndry, std::vector<double> gol,
     
     ss->setGoal(goal);
     
-    ob::PlannerPtr planner(new oc::MAPSRRT(ss->getSpaceInformation(),
-        NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius));
-    
-    // ob::PlannerPtr planner(new oc::RRT(ss->getSpaceInformation()));
+    // ob::PlannerPtr planner(new oc::MAPSRRT(ss->getSpaceInformation(),
+    //     NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius));
 
-    ss->setPlanner(planner);
+    if (planner == "RRT")
+    {
+      ob::PlannerPtr planner(new oc::RRT(ss->getSpaceInformation()));
 
-    ss->setup();
+      ss->setPlanner(planner);
 
-    // ss->print(std::cout);
+      ss->setup();
 
-    ob::PlannerStatus solved = ss->solve(planningTime);
+      ob::PlannerStatus solved = ss->solve(planningTime);
 
-    // if (solved)
-    // {
-    //   std::cout << "Found solution:" << std::endl;
+      if (solved)
+      {
+        std::cout << "Found solution:" << std::endl;
 
-    //   oc::PathControl path = ss->getSolutionPath();
-    //   std::ofstream PathFile;
-    //   PathFile.open("txt/path.txt");
-    //   if (PathFile.fail())
-    //   {
-    //     std::cerr << "ERROR: Could not open path.txt" << std::endl;
-    //     exit(1);
-    //   }
-    //   else
-    //   {
-    //     std::cout << "Writing solution to path.txt" << std::endl;
-    //     // PathFile << data << std::endl;
-    //     path.printAsMatrix(PathFile);
-    //     PathFile.close();
-    //     std::cout << "Computation completed successfully" << std::endl;
-    //     // path.print(std::cout);  // this prints out the solution
-    //   }
-    // }
+        oc::PathControl path = ss->getSolutionPath();
+        std::ofstream PathFile;
+        PathFile.open("txt/path.txt");
+        if (PathFile.fail())
+        {
+          std::cerr << "ERROR: Could not open path.txt" << std::endl;
+          exit(1);
+        }
+        else
+        {
+          std::cout << "Writing solution to path.txt" << std::endl;
+          // PathFile << data << std::endl;
+          path.printAsMatrix(PathFile);
+          PathFile.close();
+          std::cout << "Computation completed successfully" << std::endl;
+          // path.print(std::cout);  // this prints out the solution
+        }
+      }
+    }
+
+    else if (planner == "MAPSRRT")
+    {
+      ob::PlannerPtr planner(new oc::MAPSRRT(ss->getSpaceInformation(),
+        NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius, benchmark));
+
+      ss->setPlanner(planner);
+
+      ss->setup();
+
+      ob::PlannerStatus solved = ss->solve(planningTime);
+    }
+
+    else if (planner == "MAPSRRTmotion")
+    {
+      ob::PlannerPtr planner(new oc::MAPSRRTmotion(ss->getSpaceInformation(),
+        NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius, benchmark));
+
+      ss->setPlanner(planner);
+
+      ss->setup();
+
+      ob::PlannerStatus solved = ss->solve(planningTime);
+    }
+
+    else if (planner == "RRTplus")
+    {
+      ob::PlannerPtr planner(new oc::RRTplus(ss->getSpaceInformation(), gol, NumberofVehicles, 
+        NumberofControls, Radius));
+
+      ss->setPlanner(planner);
+
+      ss->setup();
+
+      ob::PlannerStatus solved = ss->solve(planningTime);
+
+    }
+
+    else
+      std::cout << "Incorrect planner selected... exiting." << std::endl;
 }
 
-// void benchmark(std::vector<double> bndry, std::vector<double> gol, 
-//     std::vector<double> obs, std::vector<double> start, std::string model, 
-//     const double planningTime, const double Tollorance, std::vector<std::string> data, 
-//     const int NumberofVehicles, const int DimensionOfVehicle, int MaxSegments, int numRuns, 
-//     const char* logName)
-// {
-//   //  set up the space using simple setup
-//   oc::SimpleSetupPtr ss;
-//   CreateSimpleSetup(ss, bndry, gol, start, model, Tollorance);
+void benchmark(std::vector<double> bndry, std::vector<double> gol, 
+    std::vector<double> obs, std::vector<double> start, std::string model, 
+    const double planningTime, const double Radius, std::vector<std::string> data, 
+    const int NumberofVehicles, const int NumberofControls, const int DimensionOfVehicle, 
+    int MaxSegments, bool benchmark, int numRuns, const char* logName)
+{
+  //  set up the space using simple setup
+  oc::SimpleSetupPtr ss;
+  CreateSimpleSetup(ss, bndry, gol, start, model, Radius);
 
-//   const oc::SpaceInformationPtr si = ss->getSpaceInformation();
+  const oc::SpaceInformationPtr si = ss->getSpaceInformation();
 
-//   if (model == "KinematicCar")
-//   {
-//     ss->setStateValidityChecker(std::make_shared<KinematicCarStateValidityChecker>(
-//       si, obs));
-//   }
-//   else if (model == "2KinematicCars")
-//   {
-//     ss->setStateValidityChecker(std::make_shared<TwoKinematicCarStateValidityChecker>(
-//       si, obs));
-//   }
+  if (model == "KinematicCar")
+  {
+    ss->setStateValidityChecker(std::make_shared<KinematicCarStateValidityChecker>(
+      si, obs));
+  }
+  else if (model == "2KinematicCars")
+  {
+    ss->setStateValidityChecker(std::make_shared<TwoKinematicCarStateValidityChecker>(
+      si, obs));
+  }
 
-//   ob::GoalPtr goal (new MyArbitraryGoal(ss->getSpaceInformation(), gol, Tollorance));
+  ob::GoalPtr goal (new MyArbitraryGoal(ss->getSpaceInformation(), gol, Radius));
     
-//   ss->setGoal(goal);
+  ss->setGoal(goal);
 
-//   // initialize the benchmark object
-//   ompl::tools::Benchmark b(*(ss.get()), "my experiment");
+  // initialize the benchmark object
+  ompl::tools::Benchmark b(*(ss.get()), "my experiment");
 
-//   // initialize the three planners
-//   ob::PlannerPtr MapsRrt(new oc::MAPSRRT(ss->getSpaceInformation(), 
-//         NumberofVehicles, DimensionOfVehicle, MaxSegments, gol));
+  // initialize the three planners
+  ob::PlannerPtr MapsRrt(new oc::MAPSRRT(ss->getSpaceInformation(),
+        NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius, benchmark));
 
-//   ob::PlannerPtr Rrt(new oc::RRT(ss->getSpaceInformation()));
+  ob::PlannerPtr MapsRrtMotion(new oc::MAPSRRTmotion(ss->getSpaceInformation(),
+        NumberofVehicles, NumberofControls, DimensionOfVehicle, MaxSegments, gol, Radius, benchmark));
+  
+  ob::PlannerPtr Rrt(new oc::RRT(ss->getSpaceInformation()));
 
-//   ob::PlannerPtr RrtPlus(new oc::RRTplus(ss->getSpaceInformation(), gol));
-
-//   // add the planners to the benchmark
-//   b.addPlanner(MapsRrt);
-//   b.addPlanner(Rrt);
-//   b.addPlanner(RrtPlus);
-
-//   // define parameters of the benchmark
-//   ompl::tools::Benchmark::Request req;
-//   req.maxTime = planningTime;
-//   req.maxMem = 1000.0;
-//   req.runCount = numRuns;
-//   req.displayProgress = true;
-
-//   // After the Benchmark class is defined, the events can be optionally registered:
-//   b.benchmark(req);
-
-//   // This will generate a file of the form ompl_host_time.log
-//   char buffer [1024];
-//   int n = sprintf (buffer, "./benchmarking_results/%s.log", logName);
-//   b.saveResultsToFile(buffer);
-
-//   // b.saveResultsToFile("./benchmarking_results/test.log");
-
-//   return; 
+  ob::PlannerPtr RrtPlus(new oc::RRTplus(ss->getSpaceInformation(), gol, NumberofVehicles,
+    NumberofControls, Radius));
 
 
-// }
+  // add the planners to the benchmark
+  // b.addPlanner(MapsRrt);
+  // b.addPlanner(MapsRrtMotion);
+  b.addPlanner(Rrt);
+  b.addPlanner(RrtPlus);
+
+  // define parameters of the benchmark
+  ompl::tools::Benchmark::Request req;
+  req.maxTime = planningTime;
+  req.maxMem = 1000.0;
+  req.runCount = numRuns;
+  req.displayProgress = true;
+
+  // After the Benchmark class is defined, the events can be optionally registered:
+  b.benchmark(req);
+
+  // This will generate a file of the form results.log
+  char buffer [1024];
+  int n = sprintf (buffer, "./benchmarking_results/%s.log", logName);
+  b.saveResultsToFile(buffer);
+
+  // b.saveResultsToFile("./benchmarking_results/test.log");
+
+  return; 
 
 
-
-
+}
 
 
 int main(int /*argc*/, char ** /*argv*/)
 {
     std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
     // Define the multi-agent problem from world file
+    std::string Task;
+    std::string Planner;
     std::string DynModel;
     int dim;
     int NumVs;
@@ -460,25 +417,66 @@ int main(int /*argc*/, char ** /*argv*/)
     // need to add this to read file eventually
     int MaxSegments;
 
-    std::cout << "Enter the maximum solve time (seconds): ";
-    std::cin >> SolveTime;
-
-    std::cout << "Enter the radius of each vehicles goal region: ";
-    std::cin >> Tollorance;
-
-    std::cout << "Enter the maximum number of segments (should be integer): ";
-    std::cin >> MaxSegments;
-
-    plan(bndry, gol, obs, strt, DynModel, SolveTime, Tollorance, data, NumVs, NumCs, dim, MaxSegments);
-
-    // int NumberofBenchmarkRuns = 100;
-
-    // const char* name = "results";
-
-    // benchmark(bndry, gol, obs, strt, DynModel, SolveTime, Tollorance, data, NumVs, dim, MaxSegments, 
-    //   NumberofBenchmarkRuns, name);
+    int NumberofBenchmarkRuns;
 
 
+    std::cout << "Would you like to plan with one planner or benchmark all planners?" << std::endl;
+    std::cout << "Choices: plan, or benchmark" << std::endl;
+    std::cin >> Task;
+
+    if (Task == "plan")
+    {
+      bool bench = false;
+
+      std::cout << "Enter the planner you wish to use." << std::endl;
+      std::cout << "Choices: MAPSRRT, MAPSRRTmotion, RRT, or RRTplus" << std::endl;
+      std::cout << "Enter: ";
+      std::cin >> Planner;
+
+      std::cout << "Enter the maximum solve time (seconds): ";
+      std::cin >> SolveTime;
+
+      std::cout << "Enter the radius of each vehicles goal region: ";
+      std::cin >> Tollorance;
+
+      if (Planner == "RRT" || Planner == "RRTplus")
+        MaxSegments = 1;
+      else
+      {
+        std::cout << "Enter the maximum number of segments (should be integer): ";
+        std::cin >> MaxSegments;
+      }
+
+    
+      plan(Planner, bndry, gol, obs, strt, DynModel, SolveTime, Tollorance, data, 
+        NumVs, NumCs, dim, MaxSegments, bench);
+    }
+
+    else if (Task == "benchmark")
+    {
+      bool bench = true;
+      
+      std::cout << "Enter number of planning runs (must be integer): ";
+      std::cin >> NumberofBenchmarkRuns;
+
+      std::cout << "Enter the maximum solve time (seconds): ";
+      std::cin >> SolveTime;
+
+      std::cout << "Enter the maximum number of segments for MAPS planners (should be integer): ";
+        std::cin >> MaxSegments;
+
+      std::cout << "Enter the radius of each vehicles goal region: ";
+      std::cin >> Tollorance;
+
+      const char* name = "results";
+
+      benchmark(bndry, gol, obs, strt, DynModel, SolveTime, Tollorance, data, NumVs, NumCs, dim, 
+        MaxSegments, bench, NumberofBenchmarkRuns, name);
+
+    }
+
+    else
+      std::cout << "No meaningful task selected... exiting.";
 
     return 0;
 }
